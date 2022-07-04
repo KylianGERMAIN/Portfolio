055efc9c1d2a33f17546dbfe4fee52b99ae498ef
KylianGERMAIN <63394149+KylianGERMAIN@users.noreply.github.com>
2022-07-04 14:13:44 +0200

    [ADD] sent contact

--- components/element/Contact.tsx
+++ components/element/Contact.tsx
@@ -1,31 +1,121 @@
 /* eslint-disable @next/next/no-img-element */
 import { NextPage } from "next"
+import { useState } from "react"
+import Sendmail from "../../pages/api/contact"
 
 
 const Contact: NextPage = () => {
+
+
+  const [Name, setName] = useState('')
+  const [Phone, setPhone] = useState('')
+  const [Email, setEmail] = useState('')
+  const [Subject, setSubject] = useState('')
+  const [Message, setMessage] = useState('')
+
+  const [Send, setSend] = useState(false)
+  const [Error, setError] = useState(0)
+
+  function reset() {
+    setName("")
+    setPhone("")
+    setEmail("")
+    setSubject("")
+    setMessage("")
+  }
 
   return (
     <div id="Contact" className=" md:flex  flex-row-reverse items-center pt-20 pb-40 justify-end p-10 space-y-10 md:space-y-0">
       <div className="lg:w-1/2 ">
         <div className="lg:w-3/4 shadow-2xl rounded-lg pt-10 ">
           <div className="pl-10 pr-20">
-            <div className="grid xl:flex justify-between space-y-10 xl:space-y-0">
-              <input className="bg-white drop-shadow-xl h-[60px] w-[200px] rounded-lg placeholder:text-[#C4C4C4] pl-[14px] focus:outline-0" type="text" placeholder="Name" />
-              <input className="bg-white drop-shadow-xl h-[60px] w-[200px] rounded-lg placeholder:text-[#C4C4C4] pl-[14px] focus:outline-0" type="text" placeholder="Phone number" />
+            <div className="grid xl:flex xl:space-x-2  space-y-10 xl:space-y-0">
+              <input className="bg-white drop-shadow-xl h-[60px] w-[200px] rounded-lg placeholder:text-[#C4C4C4] pr-[14px] pl-[14px] focus:outline-0" type="text" placeholder="Name" onChange={(e) => {
+                setName(e.target.value)
+              }} value={Name} />
+              <input className="bg-white drop-shadow-xl h-[60px] w-[200px] rounded-lg placeholder:text-[#C4C4C4] pr-[14px] pl-[14px] focus:outline-0" type="text" placeholder="Phone number" onChange={(e) => {
+                setPhone(e.target.value)
+              }} value={Phone} />
             </div>
           </div>
-          <div className="pt-10 pr-32 pl-10">
-            <input className="bg-white drop-shadow-xl h-[60px] w-full rounded-lg placeholder:text-[#C4C4C4] pl-[14px] focus:outline-0" type="text" placeholder="Email address" />
-          </div>
-          <div className="pt-10 pr-32 pl-10">
-            <input className="bg-white drop-shadow-xl h-[60px] w-full rounded-lg placeholder:text-[#C4C4C4] pl-[14px] focus:outline-0" type="text" placeholder="Subject" />
-          </div>
+          {Error != 1 ?
+            <div>
+              <div className="pt-10 pr-10 xl:pr-32 pl-10">
+                <input className="bg-white drop-shadow-xl h-[60px] w-full rounded-lg placeholder:text-[#C4C4C4] pr-[14px] pl-[14px] focus:outline-0" type="text" placeholder="Email address*" onChange={(e) => {
+                  setEmail(e.target.value)
+                }} value={Email} />
+              </div>
+              <div className="pt-10 pr-10 xl:pr-32 pl-10">
+                <input className="bg-white drop-shadow-xl h-[60px] w-full rounded-lg placeholder:text-[#C4C4C4] pr-[14px] pl-[14px] focus:outline-0" type="text" placeholder="Subject*" onChange={(e) => {
+                  setSubject(e.target.value)
+                }} value={Subject} />
+              </div>
+            </div>
+            : <div>
+              <div className="pt-10 pr-10 xl:pr-32 pl-10">
+                <input className="bg-white h-[60px] w-full rounded-lg placeholder:text-[#C4C4C4] pr-[14px] pl-[14px] border-red-500 focus:outline-0 border-red-600 border-2" type="text" placeholder="Email address*" onChange={(e) => {
+                  setEmail(e.target.value)
+                }} value={Email} />
+                <p className="text-red-600">This field is required.</p>
+              </div>
+              <div className="pt-10 pr-10 xl:pr-32 pl-10">
+                <input className="bg-white h-[60px] w-full rounded-lg placeholder:text-[#C4C4C4] pr-[14px] pl-[14px] focus:outline-0   border-red-600 border-2" type="text" placeholder="Subject*" onChange={(e) => {
+                  setSubject(e.target.value)
+                }} value={Subject} />
+                <p className="text-red-600">This field is required.</p>
+              </div>
+            </div>}
           <div className="pt-10 pr-20 pl-10">
-            <textarea className="w-full drop-shadow-xl h-[160px] resize-none rounded-lg placeholder:text-[#C4C4C4] pl-[14px] pt-[14px] focus:outline-0" id="story" name="story" placeholder="Message" >
+            <textarea className="w-full drop-shadow-xl h-[160px] resize-none rounded-lg placeholder:text-[#C4C4C4] pr-[14px] pl-[14px] pt-[14px] focus:outline-0" id="story" name="story" placeholder="Message" onChange={(e) => {
+              setMessage(e.target.value)
+            }} value={Message}>
             </textarea>
           </div>
-          <div className="p-10">
-            <button className="transition drop-shadow-xl duration-500 hover:bg-[#ff384b] bg-[#FF4C60] font-bold text-white p-1 px-6 text-xl rounded-xl">Submit</button>
+          <div className="p-10 block">
+
+            {Error != 2 ?
+              <button className="transition drop-shadow-xl duration-500 hover:bg-[#ff384b] bg-[#FF4C60] font-bold text-white p-1 px-6 text-xl rounded-xl"
+                onClick={() => {
+                  if (Email.length != 0 && Subject.length != 0) {
+                    fetch("/api/contact", {
+                      method: "POST",
+                      headers: {
+                        Accept: "application/json, text/plain, */*",
+                        "Content-Type": "application/json",
+                      },
+                      body: JSON.stringify({
+                        Name: Name,
+                        Phone: Phone,
+                        Email: Email,
+                        Subject: Subject,
+                        Message: Message
+                      }),
+                    })
+                      .then((res) => {
+                        console.log("Response received", res);
+                        if (res.status === 200) {
+                          setSend(true)
+                          setError(0)
+                          reset()
+                        } else {
+                          console.log("Email/Password is invalid.");
+                          setError(2)
+                        }
+                      })
+                      .catch((e) => console.log(e));
+                  }
+                  else {
+                    setError(1)
+                  }
+
+                }}
+              >Submit
+              </button>
+              : <button className="transition drop-shadow-xl cursor-default	 duration-500 hover:bg-[#f90000] bg-[#f90000] font-bold text-white p-1 px-6 text-xl rounded-xl"
+              >Error
+              </button>}
+
+
           </div>
         </div>
       </div>
---
+++ pages/api/contact.js
@@ -0,0 +1,37 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default async function (req, res) {
+  require('dotenv').config()
+
+  let nodemailer = require('nodemailer')
+  const transporter = nodemailer.createTransport({
+    host: "smtp-mail.outlook.com",
+    secureConnection: false,
+    port: 587,
+    auth: {
+      user: 'kylian.germain@hotmail.com',
+      pass: process.env.PASSWORDMAIL,
+    },
+  })
+
+
+  const mailData = {
+    from: 'kylian.germain@hotmail.com',
+    to: 'kylian.germain@hotmail.com',
+    subject: `${req.body.Subject.length != 0 ? req.body.Subject : `Message From ${req.body.Name}`}`,
+    text: req.body.Message + " | Sent from: " + req.body.Email + " | Phone number:" + req.body.Phone,
+    html: `<div>
+    ${req.body.Message}</div >
+   <p>Sent from: ${req.body.Email}</p>
+   <p>Phone from: ${req.body.Phone}</p>`
+  }
+
+  transporter.sendMail(mailData, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.send("error" + JSON.stringify(err));
+    } else {
+      console.log('Email sent: ' + data.response);
+      res.send(req);
+    }
+  });
+}
