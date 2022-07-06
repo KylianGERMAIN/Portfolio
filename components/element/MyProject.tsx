04b9aef628f310952c56c7f266860ad9b0c3c735
KylianGERMAIN <63394149+KylianGERMAIN@users.noreply.github.com>
2022-07-06 08:43:02 +0200

    [FIX] viepest link

--- components/element/MyProject.tsx
+++ components/element/MyProject.tsx
@@ -36,7 +36,7 @@ const MyProject: NextPage = () => {
       <h2 className="text-4xl font-bold pt-5 pb-5 text-center text-[#282828]">My project</h2>
       <div className="grid  md:grid-rows-2 md:grid-flow-col justify-center place-items-center items-center pt-10">
         {imageContainer("/img/area.png", "https://github.com/KylianGERMAIN/Area", "Area", "This is a reaction action project with APIs. The project was elected best project in the class")}
-        {imageContainer("/img/viepest.png", "https://viepest.herokuapp.com/", "ViePest", "This project was created by myself during an internship and is linked to a strapi back office")}
+        {imageContainer("/img/viepest.png", "http://viepest.com/", "ViePest", "This project was created by myself during an internship and is linked to a strapi back office")}
         {imageContainer("/img/freshen.png", "https://freshen.fr/", "Freshen", "This project is my innovative project of end Epitech it is to return for 2024 and has for goal to value the world of the second hand")}
         {imageContainerBIG("/img/Jam.png", "https://github.com/KylianGERMAIN/EPITECH_JAM_2_2021", "JAM", "This is a game I was able to do in group in 48 hours")}
       </div>
