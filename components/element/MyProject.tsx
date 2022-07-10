be5d239f88f9abc27033293ac9b8d2a3f78d90a4
KylianGERMAIN <63394149+KylianGERMAIN@users.noreply.github.com>
2022-07-10 16:08:56 +0200

    [ADD] cloudApp project

--- components/element/MyProject.tsx
+++ components/element/MyProject.tsx
@@ -34,11 +34,11 @@ const MyProject: NextPage = () => {
   return (
     <div id="My_project" className="py-32 ">
       <h2 className="text-4xl font-bold pt-5 pb-5 text-center text-[#282828]">My project</h2>
-      <div className="grid  md:grid-rows-2 md:grid-flow-col justify-center place-items-center items-center pt-10">
+      <div className="grid  md:grid-rows-2 xl:grid-rows-1 md:grid-flow-col justify-center place-items-center items-center pt-10">
         {imageContainer("/img/area.png", "https://github.com/KylianGERMAIN/Area", "Area", "This is a reaction action project with APIs. The project was elected best project in the class")}
-        {imageContainer("/img/viepest.png", "http://viepest.com/", "ViePest", "This project was created by myself during an internship and is linked to a strapi back office")}
+        {imageContainer("/img/viepest.png", "http://viepest.com/", "ViePest", "This website was created by myself during an internship and is linked to a strapi back office")}
         {imageContainer("/img/freshen.png", "https://freshen.fr/", "Freshen", "This project is my innovative project of end Epitech it is to return for 2024 and has for goal to value the world of the second hand")}
-        {imageContainerBIG("/img/Jam.png", "https://github.com/KylianGERMAIN/EPITECH_JAM_2_2021", "JAM", "This is a game I was able to do in group in 48 hours")}
+        {imageContainer("/img/Cloudapp.png", "https://github.com/KylianGERMAIN/CloudApp", "CloudApp", "This is a simple weather application connected to the open weather api")}
       </div>
     </div>
   )
